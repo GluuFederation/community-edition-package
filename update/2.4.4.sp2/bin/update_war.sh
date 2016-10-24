e6b91d691e9d10fe722c7a16e85c04dd21750afd
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2016-10-24 17:47:58 +0300

    Reffactor update war script

--- update/2.4.4.sp2/bin/update_war.sh
+++ update/2.4.4.sp2/bin/update_war.sh
@@ -7,43 +7,35 @@ TOMCAT_DIR="/opt/tomcat/webapps/"
 BACKUP_TIME=`date +%Y-%m-%d.%H:%M:%S`
 BACKUP_FOLDER="/opt/upd/2.4.4.sp1/backup_$BACKUP_TIME"
 
+apply_update () {
+    APP_NAME=$1
+    if [ -f $TOMCAT_DIR/$APP_NAME.war ]; then
+        echo "Updating $APP_NAME...."
+        if [ -d $TOMCAT_DIR/$APP_NAME ]; then
+            mv $TOMCAT_DIR/$APP_NAME $BACKUP_FOLDER
+        fi
+        mv $TOMCAT_DIR/$APP_NAME.war $BACKUP_FOLDER
+        cp $UPD_DIR/$APP_NAME.war $TOMCAT_DIR
+    fi
+}
+
+apply_idp_update () {
+    if [ -f $OXIDP/idp.war ]; then
+        echo "Updating IDP..."
+        mv $OXIDP/idp.war $BACKUP_FOLDER
+        cp $UPD_DIR/idp.war $OXIDP
+    fi
+}
 echo "Starting the update process..."
 service tomcat stop
 
 mkdir -p $BACKUP_FOLDER
 
-if [ -f $TOMCAT_DIR/oxauth.war ]; then
-    echo "Updating oxAuth...."
-    if [ -d $TOMCAT_DIR/oxauth ]; then
-        mv $TOMCAT_DIR/oxauth $BACKUP_FOLDER
-    fi
-    mv $TOMCAT_DIR/oxauth.war $BACKUP_FOLDER
-    cp $UPD_DIR/oxauth.war $TOMCAT_DIR
-fi
+apply_update oxauth
+apply_update identity
+apply_update cas
+apply_idp_update
 
-if [ -f $OXIDP/idp.war ]; then
-    echo "Updating IDP..."
-    mv $OXIDP/idp.war $BACKUP_FOLDER
-    cp $UPD_DIR/idp.war $OXIDP
-fi
-
-if [ -f $TOMCAT_DIR/cas.war ]; then
-    echo "Updating Cas..."
-    if [ -d $TOMCAT_DIR/cas ]; then
-        mv $TOMCAT_DIR/cas $BACKUP_FOLDER
-    fi
-    mv $TOMCAT_DIR/cas.war $BACKUP_FOLDER
-    cp $UPD_DIR/oxcas.war $TOMCAT_DIR/cas.war
-fi
-
-if [ -f $TOMCAT_DIR/identity.war ]; then
-    echo "Updating Identify..."
-    if [ -d $TOMCAT_DIR/identity ]; then
-        mv $TOMCAT_DIR/identity $BACKUP_FOLDER
-    fi
-    mv $TOMCAT_DIR/identity.war $BACKUP_FOLDER
-    cp $UPD_DIR/identity.war $TOMCAT_DIR
-fi
 
 service tomcat start
 echo "Successfully updated"
