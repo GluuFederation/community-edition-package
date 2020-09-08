3933af27e004b8ccf6297a3b04ccf8093c0a9de3
Mustafa Baser <mbaser@mail.com>
2020-09-08 20:14:32 +0300

    fix dependenceies.sh for centos8

--- ce-host/4.2.1/dependencies.sh
+++ ce-host/4.2.1/dependencies.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-if command -v yum > /dev/null 2>&1; then
+if ! [ -z $(grep -i "centos-7" /etc/os-release) ]
+then
     yum install -y epel-release
-
     if !  command -v python3 > /dev/null 2>&1 ; then
         yum install -y python3
     fi
-
     yum install -y python36-six python3-ldap3 python3-requests
 
+fi
 
-else
-    echo "Do apt commands"
+if ! [ -z $(grep -i "centos-8" /etc/os-release) ]
+then
+    yum install -y epel-release
+    if !  command -v python3 > /dev/null 2>&1 ; then
+        yum install -y python3
+    fi
+    yum install -y python3-six python3-ldap3 python3-requests
 
 fi
 
